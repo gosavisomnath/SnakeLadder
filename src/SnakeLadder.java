e14260c7b3a583b23db800f7d6a41cf67cb2123b
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-07-13 12:16:49 +0530

    Added Player With Position 0

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,8 +1,10 @@
 
 public class SnakeLadder {
 	public static void main(String[] args){
-		
+		int Player1Position = 0;
 		System.out.println("Welcome to Snake and Ladder game");
-		
+		int PlayerPosition = 0;
+		System.out.println("Game begins Player Position is: "+PlayerPosition);
+
 }
 }
