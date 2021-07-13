912da90960402a19f9df4747dabea0f87eea6c9c
gosavisomnath <somnathgosavi29.sg@gmail.com>
2021-07-13 12:34:09 +0530

    Added Random Function

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -5,6 +5,11 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake and Ladder game");
 		int PlayerPosition = 0;
 		System.out.println("Game begins Player Position is: "+PlayerPosition);
-
+		DiceThrow();
+}
+		public static void DiceThrow(){
+			int Dice = (int)Math.floor(Math.random()*10)%6+1;
+			System.out.println("You got Dice Value "+Dice);
+			
 }
 }
